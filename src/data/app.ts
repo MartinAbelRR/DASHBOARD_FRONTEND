d6c6e044cf193fe5ddb4b2a1485e1332c352ae62
Martin Reyes <martin.reyes@unmsm.edu.pe>
2024-03-23 21:57:42 -0500

    Las cosas tienen su final, nada dura para siempre, pero siempre es un nuevo inicio.

--- src/data/app.ts
+++ src/data/app.ts
@@ -19,11 +19,18 @@ export const itemsSideBarTop: Item[] = [
 ]
 
 export const GET_indicadores = async () => {
-    const indicadores = await fetch('http://127.0.0.1:8000/machine_learning/indicadores_totales/')
+    const indicadores = await fetch('https://dashboard-backend-kbgq.onrender.com/machine_learning/indicadores_totales')
     const indicadoresJson: Indicador[] = await indicadores.json()
 
     return indicadoresJson 
 }
+
+export const GET_sucursales_mensuales = async () => {
+    const sucursales = await fetch('https://dashboard-backend-kbgq.onrender.com/machine_learning/sucursales_x_ventas')
+    const sucursalesJson = await sucursales.json()
+
+    return sucursalesJson
+}  
 
 export const GET_ventas_mensuales = async () => {
     const ventas = await fetch('http://127.0.0.1:8000/machine_learning/ventas_mensual/')
