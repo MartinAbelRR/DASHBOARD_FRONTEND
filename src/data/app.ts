b0abbd1e46e74a8dc24887ebf1b92a6ea01fab12
Martin Reyes <martin.reyes@unmsm.edu.pe>
2024-03-23 22:03:26 -0500

    Ahora sí se termino para siempre, igual estoy feliz de aprender cosas de la fuck life my king.

--- src/data/app.ts
+++ src/data/app.ts
@@ -19,21 +19,21 @@ export const itemsSideBarTop: Item[] = [
 ]
 
 export const GET_indicadores = async () => {
-    const indicadores = await fetch('https://dashboard-backend-kbgq.onrender.com/machine_learning/indicadores_totales')
+    const indicadores = await fetch('https://dashboard-backend-kbgq.onrender.com/machine_learning/indicadores_totales/')
     const indicadoresJson: Indicador[] = await indicadores.json()
 
     return indicadoresJson 
 }
 
 export const GET_sucursales_mensuales = async () => {
-    const sucursales = await fetch('https://dashboard-backend-kbgq.onrender.com/machine_learning/sucursales_x_ventas')
+    const sucursales = await fetch('https://dashboard-backend-kbgq.onrender.com/machine_learning/sucursales_x_ventas/')
     const sucursalesJson = await sucursales.json()
 
     return sucursalesJson
 }  
 
 export const GET_ventas_mensuales = async () => {
-    const ventas = await fetch('http://127.0.0.1:8000/machine_learning/ventas_mensual/')
+    const ventas = await fetch('https://dashboard-backend-kbgq.onrender.com/machine_learning/ventas_mensual/')
     const ventasJson = await ventas.json()
 
     return ventasJson
